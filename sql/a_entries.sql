ab86a9e77dee798ea8ff2c50e48748a2943d7377
Timo Kokkonen <tjko@iki.fi>
2001-04-17 07:51:07 +0000

    test

--- sql/a_entries.sql
+++ sql/a_entries.sql
@@ -7,12 +7,8 @@
 
 CREATE TABLE a_entries (
       id	   SERIAL PRIMARY KEY, /* unique ID */
-      type         INT4 NOT NULL, /* type:
-					1=zone,
-					2=host */
-      ref	   INT4 NOT NULL, /* ptr to table specified by type field
-					-->zones.id
-					-->hosts.id */
+      host	   INT4 NOT NULL, /* ptr to hosts table id
+					-->hosts.id  */
 
       ip	   INET, /* IP number */
       reverse	   BOOL DEFAULT true, /* generate reverse (PTR) record flag */
