86e641e0806023cf8a73dcffe38046789147585d
Timo Kokkonen <tjko@iki.fi>
2001-10-18 17:37:03 +0000

    added email field to users table

--- sql/users.sql
+++ sql/users.sql
@@ -9,10 +9,10 @@ CREATE TABLE users (
 	id		SERIAL PRIMARY KEY, /* unique ID */
 	gid		INT4 DEFAULT -1, /* ptr to user group 
 					    -->user_groups.id */
-	username	TEXT UNIQUE NOT NULL CHECK(username <> ''), /* login
-									name */
+	username	TEXT NOT NULL CHECK(username <> ''), /* login name */
 	password	TEXT, /* encrypted password (MD5) */
 	name		TEXT, /* long user name */
+	email		TEXT, /* user email address */
 	superuser	BOOL DEFAULT false, /* superuser flag */
 	server		INT4 DEFAULT -1, /* default server id */
 	zone		INT4 DEFAULT -1, /* default zone id */
@@ -20,6 +20,8 @@ CREATE TABLE users (
 	last_pwd	INT4 DEFAULT 0, /* las password change time */
 	search_opts	TEXT, /* default search options */
 
-	comment	    TEXT
+	comment		TEXT,
+
+	CONSTRAINT	username_key UNIQUE(username)
 ) INHERITS(common_fields);
 
