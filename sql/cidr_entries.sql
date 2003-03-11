11eaa9781adef9873b40c5cec1f40c67c8e82ec6
Timo Kokkonen <tjko@iki.fi>
2003-03-11 13:51:00 +0000

    reserved type=13

--- sql/cidr_entries.sql
+++ sql/cidr_entries.sql
@@ -8,18 +8,19 @@
 CREATE TABLE cidr_entries (
 	id	    SERIAL PRIMARY KEY, /* unique ID */
 	type        INT4 NOT NULL, /* type:
-				      1=server (allow-transfer)
-				      2=zone (allow-update)
-				      3=zone (masters) 
-				      4=zone (allow-query) 
-				      5=zone (allow-transfer) 
-				      6=zone (also-notify) 
-				      7=server (allow-query) 
-				      8=server (allow-recursion) 
-				      9=server (blackhole) 
-				      10=server (listen-on) 
-				      11=server (forwarders)
-				      12=zone (forwarders) */
+				      1=server (allow-transfer),
+				      2=zone (allow-update),
+				      3=zone (masters),
+				      4=zone (allow-query), 
+				      5=zone (allow-transfer), 
+				      6=zone (also-notify),
+				      7=server (allow-query), 
+				      8=server (allow-recursion), 
+				      9=server (blackhole),
+				      10=server (listen-on), 
+				      11=server (forwarders),
+				      12=zone (forwarders),
+				      13=zone (transfer-source) */
         ref	    INT4 NOT NULL, /* ptr to table speciefied by type field
 					-->servers.id
 					-->zones.id  */
