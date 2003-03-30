409ff12103fb32b86bc665d1fba1cdb5962e871a
Timo Kokkonen <tjko@iki.fi>
2003-03-30 20:28:26 +0000

    now accepts WKS records with numeric protocol type (BIND 4 generates those)

--- Sauron/UtilZone.pm
+++ Sauron/UtilZone.pm
@@ -228,7 +228,9 @@ sub process_zonefile($$$$) {
     elsif ($type eq 'WKS') {
       shift @line; # get rid of IP
       fatal ("$filename($.): invalid protocol in WKS '$line[0]': $fline")
-	unless ("\U$line[0]" =~ /^(TCP|UDP)$/);
+	unless ("\U$line[0]" =~ /^(TCP|UDP|6|17)$/);
+      $line[0]='tcp' if ($line[0] = 6);
+      $line[0]='udp' if ($line[0] = 17);
       push @{$rec->{WKS}}, join(" ",@line);
     }
     elsif ($type eq 'SRV') {
