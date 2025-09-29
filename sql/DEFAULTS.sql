2d923f9b71e74087a7fb048accf61621376d4347
Michal Švamberg <svamberg@cesnet.cz>
2025-09-29 22:36:08 +0200

    Added copying of upgrade scripts for the database (#6)
    Updated database version number.

--- sql/DEFAULTS.sql
+++ sql/DEFAULTS.sql
@@ -5,9 +5,7 @@
 
 
 /* database version (do not change unless you know what you're doing!) */
-
-/* INSERT INTO settings (setting,value) VALUES('dbversion','1.3');*/
-INSERT INTO settings (setting,value) VALUES('dbversion','1.5');
+INSERT INTO settings (setting,value) VALUES('dbversion','1.8');
 
 
 /* add BIND's built-in ACLs */
